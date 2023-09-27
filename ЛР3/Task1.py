520ca77959703b0d0185e35848095ab4274bf0f5
liza <iamdadka@gmail.com>
2023-09-27 13:00:15 +0300

    Update task1

--- ЛР3/Task1.py
+++ ЛР3/Task1.py
@@ -11,7 +11,7 @@ with open('F1.txt', 'r') as f1, open('F2.txt', 'w') as f2:
         if len(words) >= 2 and words[1] in line:
             f2.write(line)
 
-if f2.__sizeof__() == 0:
+if f2.__sizeof__() != 0:
     with open('F2.txt', 'r') as f2:
         last_line = f2.readlines()[-1]
         digit_count = sum(1 for char in last_line if char.isdigit())
